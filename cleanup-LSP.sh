11919eb8997783242f42223462c51b9237620a1f
Mathias Schubanz <M.Schubanz@b-tu.de>
2019-03-16 16:16:23 +0100

    fixed Bug

--- cleanup-LSP.sh
+++ cleanup-LSP.sh
@@ -19,7 +19,7 @@ done
 
 # if the LSP has been killed, there folder should be restored with files -> delete it
 
-if [ ! -d "xtext-lsp-$LSP_NAME-$PORT" ]; then
+if [ -d "xtext-lsp-$LSP_NAME-$PORT" ]; then
 
 	rm -rf xtext-lsp-$LSP_NAME-$PORT
 
