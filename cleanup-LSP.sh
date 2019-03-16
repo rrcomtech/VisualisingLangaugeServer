f426c2933cf92975de3657d360401e843a714825
Mathias Schubanz <M.Schubanz@b-tu.de>
2019-03-16 15:41:43 +0100

    Refined cleanup mechanism

--- cleanup-LSP.sh
+++ cleanup-LSP.sh
@@ -5,7 +5,22 @@ LSP_NAME=$2
 
 while [[ ! `lsof -t -i:$PORT` ]]
 do 
-	sleep 0.5
+	sleep 1
 done
 
 rm -rf xtext-lsp-$LSP_NAME-$PORT
+
+# wait until LSP is finished
+
+while [[ `lsof -t -i:$PORT` ]]
+do 
+	sleep 10
+done
+
+# if the LSP has been killed, there folder should be restored with files -> delete it
+
+if [ ! -d "xtext-lsp-$LSP_NAME-$PORT" ]; then
+
+	rm -rf xtext-lsp-$LSP_NAME-$PORT
+
+fi
